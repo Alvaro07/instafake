089c83d8f62b70c62947996e5fde45a4a118ce8b
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-08-16 13:45:41 +0200

    add firebase

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { UserForm } from '../components/UserForm'
+import { Context } from '../Context'
 
 export const Register = () => {
-  return <UserForm title='Register' />
+  const { activateAuth } = useContext(Context)
+
+  const onRegister = data => {
+    console.log(data)
+  }
+
+  return <UserForm title="Register" onSubmit={data => onRegister(data)} />
 }

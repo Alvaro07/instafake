957984fc5ab537900aebe8494875aa2e0dd9e1d9
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-09-10 12:32:51 +0200

    Add header to homepage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import firebase from '../firebase'
 import { Link } from '@reach/router'
-import { HeaderWrap, Logo } from './styles'
+import { Context } from '../../Context'
+import { StickyHeader, Logo, Wrap } from './styles'
+import { Button } from '../Button'
 
 export const Header = () => {
+  const { removeAuth } = useContext(Context)
+
+  const signOut = () => {
+    firebase.logout()
+    removeAuth()
+  }
+
   return (
-    <HeaderWrap>
-      <Logo>
-        <Link to="/">Instafake</Link>
-      </Logo>
-    </HeaderWrap>
+    <StickyHeader>
+      <Wrap>
+        <Logo>
+          <Link to="/" component={Logo}>
+            Instafake
+          </Link>
+        </Logo>
+        <Button text="Log out" onClick={signOut} />
+      </Wrap>
+    </StickyHeader>
   )
 }
